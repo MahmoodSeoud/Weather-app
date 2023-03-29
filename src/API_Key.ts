71c20cd9127f7284de7b60329792b83e41fb50f9
MahmoodSeoud <Mahmoodseoud@gmail.com>
2023-03-29 23:19:17 +0200

    Removed console.log

--- src/API_Key.ts
+++ src/API_Key.ts
@@ -5,8 +5,6 @@ const API_BASE_URL_WEATHER: any = process.env.REACT_APP_API_BASE_URL_WEATHER;
 const API_KEY_NAME: any = process.env.REACT_APP_API_KEY_NAME;
 const API_KEY_VALUE: any = process.env.REACT_APP_API_KEY_VALUE; 
 
-console.log(API_KEY_NAME)
-
 export const APILocation = (location: string): string => {
    return `${API_BASE_URL_DIRECT}?q=${location.trim()}&limit=1&${API_KEY_NAME}=${API_KEY_VALUE}`;
 }
